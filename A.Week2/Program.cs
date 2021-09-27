d1e025fe800fc280bb74454e407597783a172437
v.cassano <val.cassa@gmail.com>
2021-09-27 09:21:20 +0200

    Add project files.

---
+++ A.Week2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace A.Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
